adf645b3194ea16f7b1994838f068ca6dd368a3f
Juli <julietasoriaa@hotmail.com>
2026-05-12 12:43:21 -0300

    Agrego scroll interactivo hacia propiedades

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const boton = document.querySelector(".boton-principal");
-
-boton.addEventListener("click", function () {
-
-    alert("Bienvenido a DIVARVARO");
-
-});
 const botonHero = document.querySelector(".boton-hero");
 
 botonHero.addEventListener("click", function () {
 
-    alert("Bienvenido a DIVARVARO");
+    const seccionPropiedades = document.querySelector("#propiedades");
+
+    seccionPropiedades.scrollIntoView({
+        behavior: "smooth"
+    });
 
 });
