0ffe50990d1cc9a1b6a5440f843e0ab43aad577d
Juli <julietasoriaa@hotmail.com>
2026-05-12 12:52:32 -0300

    Genero cards dinamicamente con JavaScript

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ botonHero.addEventListener("click", function () {
     });
 
 });
+const propiedades = [
+    {
+        titulo: "Casa moderna",
+        precio: "USD 180.000",
+        imagen: "assets/imagenes/casa.jpg"
+    },
+    {
+        titulo: "Casa Country Álamos",
+        precio: "USD 155.000",
+        imagen: "assets/imagenes/casa.jpg"
+    },
+    {
+        titulo: "Departamento con cochera",
+        precio: "USD 180.000",
+        imagen: "assets/imagenes/casa.jpg"
+    }
+];
+
+const contenedorPropiedades = document.querySelector("#contenedor-propiedades");
+
+propiedades.forEach(function (propiedad) {
+    contenedorPropiedades.innerHTML += `
+        <div class="card">
+            <img src="${propiedad.imagen}" alt="${propiedad.titulo}">
+
+            <h3>${propiedad.titulo}</h3>
+
+            <p>${propiedad.precio}</p>
+
+            <button class="boton-principal">
+                Ver más
+            </button>
+        </div>
+    `;
+});
