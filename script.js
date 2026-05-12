c08c44f808473058f1c759ba64e5c2ee118e77c0
Juli <julietasoriaa@hotmail.com>
2026-05-12 17:01:55 -0300

    Corrijo scroll del hero y organizo eventos JS

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const botonHero = document.querySelector(".boton-hero");
+
+botonHero.addEventListener("click", function () {
+    const seccionPropiedades = document.querySelector("#propiedades");
+
+    seccionPropiedades.scrollIntoView({
+        behavior: "smooth"
+    });
+});
 const propiedades = [
     {
         titulo: "Casa moderna",
