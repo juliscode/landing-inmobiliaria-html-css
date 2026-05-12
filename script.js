1e165c018a288f1e77e1b4a9e899e72accecb9ca
Juli <julietasoriaa@hotmail.com>
2026-05-12 12:29:36 -0300

    Agrego interaccion inicial con JS

---
+++ script.js
@@ -0,0 +1,14 @@
+const boton = document.querySelector(".boton-principal");
+
+boton.addEventListener("click", function () {
+
+    alert("Bienvenido a DIVARVARO");
+
+});
+const botonHero = document.querySelector(".boton-hero");
+
+botonHero.addEventListener("click", function () {
+
+    alert("Bienvenido a DIVARVARO");
+
+});
